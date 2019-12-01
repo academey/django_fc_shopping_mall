577eeb9ce2b841ac9fada0fc2489485a8a7a7e43
academey <academey@gmail.com>
2019-12-01 16:47:00 +0900

    Register templates, forms 만들기

--- fc_django/fcuser/views.py
+++ fc_django/fcuser/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
+def index(request):
+    return render(request, 'index.html')
 
-# Create your views here.
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/'
