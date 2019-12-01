a134878f69ee7d17cdbc36d66f74cd9dec401f99
academey <academey@gmail.com>
2019-12-01 14:20:37 +0900

    Add models & makemigrations & migrate

--- fc_django/fcuser/models.py
+++ fc_django/fcuser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Fcuser(models.Model):
+    email = models.EmailField(verbose_name='이메일')
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록 날짜')
+
+    class Meta:
+        db_table = 'fastcampus_fcuser'
+        verbose_name = '유저'
+        verbose_name_plural = '유저들'
--- fc_django/order/models.py
+++ fc_django/order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    fcuser = models.ForeignKey('fcuser.Fcuser', on_delete=models.CASCADE, verbose_name='사용자')  # 클래스 안의 모델을 지칭
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='제품')  # 클래스 안의 모델을 지칭
+    quantity = models.IntegerField(verbose_name='수')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록 날짜')
+
+    class Meta:
+        db_table = 'fastcampus_order'
+        verbose_name = '주문'
+        verbose_name_plural = '주문들'
