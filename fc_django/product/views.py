db280b7e0af07a24b42eda5914611b5fd463ccd9
academey <academey@gmail.com>
2019-12-17 00:01:46 +0900

    Add product detail page with DetailView

--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, DetailView
 from .models import Product
 from .forms import RegisterForm
 
@@ -14,3 +13,11 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    # 어떤 쿼리에서 찾을지 알려줘야만 한다. 만약 특정 조건이 필요하다면
+    # 아래 쿼리에서 all(필터) 를 넣어줘야 한다.
+    queryset = Product.objects.all()
+    context_object_name = 'product'
