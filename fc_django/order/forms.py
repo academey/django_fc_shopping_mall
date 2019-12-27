3421f89320a0ad0f502a4499376fb05b8672ff19
academey <academey@gmail.com>
2019-12-27 17:03:48 +0900

    request 를 OrderForm 에 넘겨주고 받아주기. 그리고 OrderCreate 클래스 뷰에 form_invalid 함수도 추가함.

--- fc_django/order/forms.py
+++ fc_django/order/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Order
-
+from product.models import Product
+from fcuser.models import Fcuser
 
 class OrderForm(forms.Form):
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
     # 여기서 fcuser와 를 받을 필요가 없다. 세션에 들고 있는 값이 있을 것이므로 그걸 쓰면 되니까!
     # 대신, product 는 HiddenInput 으로 받아줘야 한다.
     quantity = forms.IntegerField(
@@ -13,7 +18,7 @@ class OrderForm(forms.Form):
     )
     product = forms.IntegerField(
         error_messages={
-            'required': '상품을 입력해주세요'
+            'required': '상품을 입 력해주세요'
         },
         label='상품', widget=forms.HiddenInput
     )
@@ -22,7 +27,19 @@ class OrderForm(forms.Form):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
+        fcuser_email = self.request.session.get('user')
 
+        if quantity and product and fcuser_email:
+            order = Order(
+                quantity=quantity,
+                product=Product.objects.get(pk=product),
+                fcuser=Fcuser.objects.get(email=fcuser_email)
+            )
+            order.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다')
+            self.add_error('product', '값이 없습니다')
         # name = cleaned_data.get('name')
         # price = cleaned_data.get('price')
         # description = cleaned_data.get('description')
--- fc_django/order/views.py
+++ fc_django/order/views.py
@@ -1,6 +1,19 @@
 from django.views.generic.edit import FormView
 from .forms import OrderForm
+from django.shortcuts import redirect
+
 
 class OrderCreate(FormView):
     form_class = OrderForm
     success_url = '/product/'
+
+    def form_invalid(self, form):
+        return redirect('/product/' + str(form.product))
+
+    def get_form_kwargs(self, **kwargs):
+        kw = super().get_form_kwargs(**kwargs)
+        kw.update({
+            'request': self.request
+        })
+
+        return kw
